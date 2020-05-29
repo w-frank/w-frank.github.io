58bf854fad8e0df103fd04d8f3cfe5bc765742c4
Will Frank <william.frank@stfc.ac.uk>
2020-05-29 17:11:18 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Personal Site
+
+My personal site built with [Pelican]("http://getpelican.com) and [GitHub Pages]("https://pages.github.com).
+
+## Deployment
+```shell
+git clone https://github.com/w-frank/w-frank.github.io.git
+-- add content --
+publish.bat "commit message"
+```
---
+++ pelicanconf.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Will Frank'
+SITENAME = 'The Game of Logic'
+SITEURL = ''
+PATH = 'content'
+TIMEZONE = 'Europe/London'
+DEFAULT_DATE_FORMAT = '%A %d %B %Y'
+DEFAULT_LANG = 'en'
+
+# Specify a customized theme, via path relative to the settings file
+THEME = "theme/cebong"
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'))
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
