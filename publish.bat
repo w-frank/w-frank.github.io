fa83123cc7c0ea8494093dca0e84748568ae2dd7
Will Frank <william.frank@stfc.ac.uk>
2020-05-29 17:12:04 +0100

    Initial publication

--- publish.bat
+++
@@ -1,7 +0,0 @@
-git add .
-git commit -a -m %1
-git push -u --force origin pelican
-pelican content -o output -s pelicanconf.py
-ghp-import output -r origin -b master -m %1
-git push -u --force origin master
-git checkout pelican
